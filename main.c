cff8bd5fd282154d8d63ffd780c3e10df8c77523
Batuhan Sencer <dev.batuhansencer@gmail.com>
2024-10-27 16:41:27 -0500

    Defined the library's structure

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+struct library{  // Define the library data structure
+    char book_name[20];
+    char author_name[20];
+    int pages;
+    float price;
+}
+
+int main(){
+    
+}
